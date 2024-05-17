92b99f6c001df90bb21d75d5e17d0ec058261944
heexohee <rsoy2918@naver.com>
2024-05-17 16:38:58 +0900

    feat: 연락처 뷰

--- RainP/ContentView.swift
+++ RainP/ContentView.swift
@@ -1,21 +1,54 @@
-//
-//  ContentView.swift
-//  RainP
-//
-//  Created by heesohee on 5/17/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var message = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("긴급 메세지를 입력해 주세요.")
+                    .font(.headline)
+                    .foregroundColor(.green)) {
+                        ZStack(alignment: .topLeading) {
+                            if message.isEmpty {
+                                Text("ex. 일시적인 공황장애 발생, 잠시 쉴 수 있게만 도와주세요 등등....")
+                                    .foregroundColor(.gray)
+                                    .padding(.top, 8)
+                                    .padding(.leading, 5)
+                            }
+                            TextEditor(text: $message)
+                                .frame(height: 140)
+                                .opacity(message.isEmpty ? 0.25 : 1)
+                        }
+                        
+                    }
+            }
+            .navigationTitle("긴급 메세지")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        HStack {
+                            Text("저장")
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.blue)
+                        }
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("취소") {
+                        dismiss()
+                    }
+                    .foregroundColor(.blue)
+                }
+            }
         }
-        .padding()
     }
 }
 
