6f505c845f0a92cbb7380b52489d69984dc2cc1b
heexohee <rsoy2918@naver.com>
2024-05-17 09:37:47 +0900

    Initial Commit

---
+++ RainP/RainPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainPApp.swift
+//  RainP
+//
+//  Created by heesohee on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct RainPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
